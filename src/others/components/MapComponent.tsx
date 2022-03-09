0fa9e31a7754a974c34ce400c0989712bd90914e
Andrii <andriytinok@gmail.com>
2022-03-09 09:43:55 +0100

    feat: use react-map-gl instead of manual initialization of map

--- src/others/components/MapComponent.tsx
+++ src/others/components/MapComponent.tsx
@@ -1,28 +1,28 @@
-// @ts-nocheck
-import { useRef, useEffect, useState } from 'react'
-import maplibregl from 'maplibre-gl'
+import { useState } from "react";
+import Map from "react-map-gl";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
 import styles from "./MapComponent.module.css";
 
 export default function MapComponent() {
-  const mapContainer = useRef(null)
-  const map = useRef(null)
-  const [lng] = useState(30.5240501)
-  const [lat] = useState(48.4501071)
-  const [zoom] = useState(5)
-
-  useEffect(() => {
-    if (map.current) return; //stops map from intializing more than once
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL`,
-      center: [lng, lat],
-      zoom: zoom
-    })
-  })
+  const [lng] = useState(30.5240501);
+  const [lat] = useState(48.4501071);
+  const [zoom] = useState(5);
 
   return (
     <div className={styles.mapWrap}>
-      <div ref={mapContainer} className={styles.map} />
+      <div className={styles.map}>
+        <Map
+          mapLib={maplibregl}
+          initialViewState={{
+            longitude: lng,
+            latitude: lat,
+            zoom: zoom,
+          }}
+          mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+        />
+      </div>
     </div>
-  )
+  );
 }
