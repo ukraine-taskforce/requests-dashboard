0a75d0e23a5782a90e75d5269f6f339fc5944a53
JakubCiepka <jakub.ciepka@gmail.com>
2022-03-11 01:05:47 +0100

    moved css and fixed import issue

--- src/pages/requests/index.tsx
+++ src/pages/requests/index.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 
 import { useLocationsQuery } from "../../others/contexts/api";
 import { Layout } from "../../others/components/Layout";
-import { Map } from "../../others/components/Map";
+import { Map } from "../../others/components/map/Map";
 
 import { Header } from "../../others/components/Header";
 import { Main } from "../../others/components/Main";
