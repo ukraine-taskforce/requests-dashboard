ef5944ff47b01e74500913f97c10a6fbab5aeb42
Maciej Reimann <maciej.reimann@gmail.com>
2022-03-12 13:31:23 +0100

    rm unused imports

--- src/others/components/CollapsibleList.tsx
+++ src/others/components/CollapsibleList.tsx
@@ -1,17 +1,9 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer, { TableContainerProps } from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
 import { CollapsibleListItem, ListItem } from "../components/CollapsibleListItem";
 
 export interface CollapsibleTableProps extends TableContainerProps {
