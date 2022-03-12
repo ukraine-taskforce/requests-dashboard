956a75c66b0ba929821766b4b20727ad36caa136
JakubCiepka <jakub.ciepka@gmail.com>
2022-03-12 12:52:19 +0100

    Refactored slider to use array of possible dates as props.

--- src/others/components/TimelineSlider.tsx
+++ src/others/components/TimelineSlider.tsx
@@ -2,39 +2,46 @@ import { Box, Grid, Slider, Typography } from "@mui/material";
 import React from "react";
 import moment from "moment";
 
-const dateFormat = 'DD.MM'
+const dateDisplayFormat = 'DD.MM'
+const inputDateFormat = 'DD-MM-YYYY'
+export interface TimelineSliderProps {
+    // Timeline slider assums that dates are sorted and distinct,
+    // passed as a strings in format DD-MM-YYYY.
+    possibleDates: string[]
+}
+export const TimelineSlider = ({ possibleDates }: TimelineSliderProps) => {
+    const endDate = moment(possibleDates[possibleDates.length - 1], inputDateFormat)
+    const startDate = moment(possibleDates[0], inputDateFormat)
 
-export function TimelineSlider() {
-    let todayDate = moment()
-    let weekBeforeDate = moment().subtract(7, 'days')
-    const [selectedDateValue, setValue] = React.useState(moment);
-    const rangeLabel = `${weekBeforeDate.format(dateFormat)} - ${todayDate.format(dateFormat)}`
+    const [selectedDateValue, setValue] = React.useState(endDate)
+    const rangeLabel =
+        `${startDate.format(dateDisplayFormat)} - ${endDate.format(dateDisplayFormat)}`
 
-    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    const handleSliderChange = (_event: Event, newValue: number | number[]) => {
         if (typeof newValue === 'number') {
-            setValue(moment().subtract(7 - newValue, 'days'))
+            setValue(moment(possibleDates[newValue - 1], inputDateFormat))
         }
-    };
+    }
 
     return (
         <Box sx={{
-            width: 350, m:2, padding: 1, borderRadius: "20px",
+            width: 350, m: 2, padding: 1, borderRadius: "20px",
             backgroundColor: "#000",
             color: "#fff"
         }}>
             <Typography sx={{ fontSize: 16, marginLeft: 12 }}>
-                {selectedDateValue.format(dateFormat)}
+                {selectedDateValue.format(dateDisplayFormat)}
             </Typography>
             <Grid container spacing={3} alignItems="center">
                 <Grid item xs sx={{ marginLeft: 2 }}>
                     <Slider
                         aria-label="Timeline"
-                        defaultValue={7}
+                        defaultValue={possibleDates.length}
                         onChange={handleSliderChange}
                         step={1}
                         marks
                         min={1}
-                        max={7}
+                        max={possibleDates.length}
                         valueLabelDisplay="off"
                     />
                 </Grid>
