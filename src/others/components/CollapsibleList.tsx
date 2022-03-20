57a24e251b36ec6863ab8a2ccbb51d12f1d4eb81
Maciej Reimann <maciej.reimann@gmail.com>
2022-03-20 17:10:11 +0100

    scrolling works

--- src/others/components/CollapsibleList.tsx
+++ src/others/components/CollapsibleList.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableContainer, { TableContainerProps } from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import { CollapsibleListItem, ListItem } from "../components/CollapsibleListItem";
 
@@ -12,21 +14,49 @@ export interface CollapsibleTableProps extends TableContainerProps {
 }
 
 export const CollapsibleTable = ({ rows, sortRight, renderRowData, ...tableProps }: CollapsibleTableProps) => {
-  // TODO: implement proper infinite scroll
-  const visibleRows = rows.slice(0, 30);
+  const offset = 20; // TODO: consider calculate it dynamically
+  const firstBatch = rows.slice(0, offset);
+
+  const [displayedRows, setDisplayedRows] = useState(firstBatch);
+
+  const hasDisplayedAll = displayedRows.length === rows.length;
+
+  const nextRows = rows.slice(displayedRows.length, displayedRows.length + offset);
+
+  const addMoreRows = () => {
+    const newRows = displayedRows.concat(nextRows);
+    console.log("newRows", newRows);
+    setTimeout(() => {
+      setDisplayedRows(newRows);
+    }, 100);
+  };
+
   // TODO: type it properly - strings can be sorted too
-  const sorted = sortRight ? visibleRows.sort((a, b) => sortRight(Number(a.value), Number(b.value))) : visibleRows;
+  const sorted = sortRight ? displayedRows.sort((a, b) => sortRight(Number(a.value), Number(b.value))) : displayedRows;
   return (
     // TODO: add rounded corners
-    <TableContainer component={Paper} {...tableProps}>
-      <Table aria-label="collapsible table">
-        <TableBody sx={{ "& > *": { paddingY: 2 } }} className="collapsible-table-body">
-          {sorted.map((row, index) => (
-            // TODO: last item should not have border
-            <CollapsibleListItem key={`${row.name}-${index}`} {...renderRowData(row)} wrapperProps={{ paddingY: 2 }} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <InfiniteScroll
+      dataLength={displayedRows.length}
+      next={() => addMoreRows()}
+      hasMore={!hasDisplayedAll}
+      loader={<h4>Loading...</h4>}
+      scrollableTarget="scrollableDiv"
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      <TableContainer component={Paper} {...tableProps}>
+        <Table aria-label="collapsible table">
+          <TableBody sx={{ "& > *": { paddingY: 2 } }} className="collapsible-table-body">
+            {sorted.map((row, index) => (
+              // TODO: last item should not have border
+              <CollapsibleListItem key={`${row.name}-${index}`} {...renderRowData(row)} wrapperProps={{ paddingY: 2 }} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </InfiniteScroll>
   );
 };
