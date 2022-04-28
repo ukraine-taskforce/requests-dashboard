3ae6c19bf0e2351428f293243e7f8fd7db46d66f
AntonRaichuk <raveman.net@gmail.com>
2022-04-28 10:51:54 +0000

    Donations

---
+++ src/others/components/CollapsibleListSimple.tsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useMap } from "react-map-gl";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer, { TableContainerProps } from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { CollapsibleListItem, ListItem } from "../components/CollapsibleListItemSimple";
+export interface CollapsibleTableProps extends TableContainerProps {
+  rows: ListItem[];
+  renderRowData: (row: ListItem) => ListItem;
+}
+
+export interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+export const CollapsibleTable = ({ rows, renderRowData, ...tableProps }: CollapsibleTableProps) => {
+  const { t } = useTranslation();
+  const offset = 20;
+  const getGirstBatch = (rows: ListItem[]) => rows.slice(0, offset);
+
+  const [displayedRows, setDisplayedRows] = useState<ListItem[]>([]);
+  const [openItemsIds, setOpenItemsIds] = useState<string[]>([]);
+  const [selectedCity, setSelectedCity] = useState<Coordinates | undefined>(undefined);
+  const map = useMap();
+
+  const hasDisplayedAll = displayedRows.length === rows.length;
+
+  const addMoreRows = () => {
+    const nextRows = rows.slice(displayedRows.length, displayedRows.length + offset);
+    setDisplayedRows([...displayedRows, ...nextRows]);
+  };
+
+  const toggleZoomCity = (coordinates: Coordinates) => {
+    const { latitude, longitude } = coordinates;
+    if (selectedCity?.latitude === latitude && selectedCity?.longitude === longitude) {
+      setSelectedCity(undefined);
+    } else {
+      setSelectedCity(coordinates);
+    }
+  };
+
+  useEffect(() => {
+    setDisplayedRows(getGirstBatch(rows));
+  }, [rows]);
+
+  const handleOpenItem = (id: string) => {
+    const idsWithoutId = openItemsIds.filter((openId) => openId !== id);
+    const idsWithId = [...openItemsIds, id];
+    setOpenItemsIds(openItemsIds.includes(id) ? idsWithoutId : idsWithId);
+  };
+
+  useEffect(() => {
+    if (selectedCity) {
+      const { latitude, longitude } = selectedCity;
+      map.default?.flyTo({ center: [longitude, latitude], duration: 2000, zoom: 10 });
+    } else {
+      // Reset to initial map view
+      map.default?.flyTo({ center: [10.5, 48.45], duration: 2000, zoom: 5 });
+    }
+  }, [selectedCity, map]);
+
+  return (
+    <InfiniteScroll
+      dataLength={displayedRows.length}
+      next={() => addMoreRows()}
+      hasMore={!hasDisplayedAll}
+      loader={
+        <Typography variant="body2" noWrap>
+          {t("loading")}...
+        </Typography>
+      }
+      scrollableTarget="scrollableDiv"
+      // TODO: for overflowing tables add a "back to top" button
+      // endMessage={<> </>}
+    >
+      <TableContainer component={Paper} {...tableProps}>
+        <Table aria-label="collapsible table">
+          <TableBody sx={{ "& > *": { paddingY: 2 } }} className="collapsible-table-body">
+            {displayedRows.map((row, index) => {
+              // TODO: Propagate proprer id here.
+              const id = `${row.name}`;
+              return (
+                <CollapsibleListItem
+                  key={id}
+                  {...renderRowData(row)}
+                  wrapperProps={{ paddingY: 2 }}
+                  selectedCity={selectedCity}
+                  toggleZoomCity={toggleZoomCity}
+                  open={openItemsIds.includes(id)}
+                  handleClick={() => handleOpenItem(id)}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </InfiniteScroll>
+  );
+};
---
+++ src/pages/donations/index.tsx
@@ -0,0 +1,135 @@
+import { useState, useEffect } from "react";
+import MapComponent, { Popup, MapProvider, Marker } from "react-map-gl";
+
+import { Layout } from "../../others/components/Layout";
+import { Header } from "../../others/components/mckinsey/Header";
+import { Main } from "../../others/components/Main";
+import { Sidebar } from "../../others/components/Sidebar";
+import { CollapsibleTable } from "../../others/components/CollapsibleListSimple";
+import { Box, Typography } from "@mui/material";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+type DonationPoint = {
+  id: number;
+  name: string;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+  phoneNumber: string;
+  description: string;
+  openingHours: string;
+};
+
+const fakeDonationPoints: DonationPoint[] = [
+ {
+   id: 1,
+   name: "ETH drop-off location",
+   coordinates: {
+     latitude: 47.3764,
+     longitude: 8.5476,     
+   },
+   phoneNumber: "+41781111111",
+   description: "EN:\ntake only cloths\nUA:\nberem odjag",
+   openingHours: "daily 10:00-17:00 except for sundays",
+ },
+ {
+   id: 2,
+   name: "Ukrainian embassy in Bern",
+   coordinates: {
+     latitude: 46.9439,
+     longitude: 7.4471
+   },
+   phoneNumber: "+41782222222",
+   description: "DE: wir nehmen alles\nUA: prunoste vse\nEN: we take everything",
+   openingHours: "check the embassy website",
+ },
+];
+
+export function Donations() {
+  const [popupInfo, setPopupInfo] = useState<DonationPoint | null>(null);
+  
+  useEffect(() => {
+    document.title = "Donation drop-off points";
+  }, []);
+
+  const tableData = fakeDonationPoints.map((point: DonationPoint) => {
+    return {
+      name: point.name,
+      description: `${point.description}\n\nPhone number: ${point.phoneNumber}\nOpening hours: ${point.openingHours}`,
+      coordinates: point.coordinates,
+    };
+  });
+
+  const table = (
+    <CollapsibleTable
+      rows={tableData}
+      renderRowData={(row) => {
+        return {
+          name: row.name,
+          coordinates: row.coordinates,
+          description: row.description,
+        };
+      }}
+    />
+  );
+
+  const initialMapView = {
+    latitude: 48.45,
+    longitude: 10.5,
+    zoom: 5,
+  };
+
+  return (
+    <Layout header={<Header />}>
+      <MapProvider>
+        <Main
+          aside={
+            <Sidebar className="requests-sidebar">
+              {table}
+            </Sidebar>
+          }
+        >
+          <Box sx={{ height: "100%", width: "100%" }}>
+            <MapComponent
+              mapLib={maplibregl}
+              initialViewState={initialMapView}
+              mapStyle="https://api.maptiler.com/maps/streets/style.json?key=8XnO8TF3UjHDY1RKP9jm"
+              style={{ borderRadius: "24px" }}
+            >
+              {fakeDonationPoints.map((point: DonationPoint) => (
+                <Marker key={point.id.toString()} longitude={point.coordinates.longitude} latitude={point.coordinates.latitude} onClick={e => {
+                  e.originalEvent.stopPropagation();
+                  setPopupInfo(point);
+                }} />))}
+              {popupInfo && (
+               <Popup
+                 longitude={popupInfo.coordinates.longitude}
+                 latitude={popupInfo.coordinates.latitude}
+                 onClose={() => setPopupInfo(null)}
+                 closeButton={false}
+                 closeOnClick={true}
+                 style={{
+                   color: "#000000",
+                 }}
+               >
+                 <div>
+                   <Typography variant="h6" component="div">
+                     {popupInfo.name}
+                   </Typography>
+                   <pre>
+                     {popupInfo.description}
+                   </pre>
+                   <br/>
+                   <b>Phone number:</b> {popupInfo.phoneNumber}<br/>
+                   <b>Opening hours:</b> {popupInfo.openingHours}<br/>
+                </div>
+              </Popup>)}
+            </MapComponent>
+          </Box>
+        </Main>
+      </MapProvider>
+    </Layout>
+  );
+}
